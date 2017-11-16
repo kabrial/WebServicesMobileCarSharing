eeb23588ebd517da79d532ed16f0a11a36e289ae
kabrial <kevin.abrial@hotmail.fr>
2017-11-16 20:56:27 +0100

    Initial commit.

---
+++ src/main/java/com/carSharing/WebServicesMobileCarSharingApplication.java
@@ -0,0 +1,12 @@
+package com.carSharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesMobileCarSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesMobileCarSharingApplication.class, args);
+	}
+}
